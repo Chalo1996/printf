baa2314092b4ec11ae857f427b89a6504fb15859
Chalo1996 <emusyoka759@gmail.com>
2022-05-22 18:54:24 +0000

    skeleton file

--- _printf.c
+++ _printf.c
@@ -1,52 +1,53 @@
-#include "main.h"
 #include <stdarg.h>
-#include <stdio.h>
-#include <ctype.h>
+#include <stddef.h>
+#include "main.h"
 
 /**
-  * _printf - produces outoput according to a format.
-  * @format: format specifier.
+  * _printf - all logic for the printf project.
+  * @format: string specifier formats.
   *
-  * Return: number of characters.
+  * Return: The length(char_num++).
   */
 int _printf(const char *format, ...)
 {
-	int i, len_format = 0, charptr;
-	int *num = &charptr;
-	char *ch = (char *)num;
-	char *str;
+	va_list ap;
+	unsigned int i = 0, char_num = 0;
 
-	va_list args;
+	if (!format)
+		return (-1);
 
-	va_start(args, format);
-
-	while (format[len_format])
-		len_format++;
-
-	for (i = 0; i < len_format; i++)
+	va_start(ap, format);
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == 'c')
+		if (format[i] == '%')
 		{
-			charptr = va_arg(args, int);
-			fprintf(stdout, ch);
+			if (format[i + 1] == '\0')
+				return (-1);
+
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				char_num++;
+				i++;
+			}
+			else if (func(format[i + 1]) != NULL)
+			{
+				char_num += (func(format[i + 1]))(ap);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				char_num++;
+			}
 		}
-
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			fprintf(stdout, str);
-		}
-
-
-		else if (format[i] == '%')
-		{
-			fprintf(stdout, "%%");
-		}
-
 		else
-			break;
+		{
+			_putchar(format[i]);
+			char_num++;
+		}
 	}
+	return (char_num);
 
-	return (len_format - 1);
-	va_end(args);
+	va_end(ap);
 }
