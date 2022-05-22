cd1c96f5383a90c3abfae17a6793ebddc93785fd
Chalo1996 <emusyoka759@gmail.com>
2022-05-22 22:58:15 +0000

    declared buffer

--- _putchar.c
+++ _putchar.c
@@ -10,3 +10,26 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+  * buffer - buffer, save characters here.
+  * @c: character
+  *
+  * Return: 1
+  */
+int buffer(char c)
+{
+	static char buff[1024];
+	static int i;
+
+	if (c == -1 || i == 1024)
+	{
+		write(1, buff, i);
+		i = 0;
+	}
+
+	if (c != -1)
+		buff[i++] = c;
+
+	return (1);
+}
