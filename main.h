a6e9873b95bb20e692bf1329ae7b5fdaa8d9b816
Chalo1996 <emusyoka759@gmail.com>
2022-05-23 02:20:40 +0000

    added print_int and print_dec files

--- main.h
+++ main.h
@@ -16,12 +16,12 @@ typedef struct types
 } print_func;
 
 int print_c(va_list c);
-int print_dec(va_list dec);
 int print_string(va_list s);
 int (*func(const char a))(va_list);
 int print_num(va_list n);
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_int(va_list n);
+int print_dec(va_list dec);
 
 #endif /* MAIN_H */
