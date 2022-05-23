4ca323886653061ac978b9ec30edae002e943645
Chalo1996 <emusyoka759@gmail.com>
2022-05-23 01:27:24 +0000

    recovered _putchar

--- _putchar.c
+++ _putchar.c
@@ -1,35 +1,13 @@
 #include <unistd.h>
+
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
-	return (buffer(c));
-}
-
-/**
-  * buffer - buffer, save characters here.
-  * @c: character
-  *
-  * Return: 1
-  */
-int buffer(char c)
-{
-	static char buff[1024];
-	static int i;
-
-	if (c == -1 || i == 1024)
-	{
-		write(1, buff, i);
-		i = 0;
-	}
-
-	if (c != -1)
-		buff[i++] = c;
-
-	return (1);
+	return (write(1, &c, 1));
 }
